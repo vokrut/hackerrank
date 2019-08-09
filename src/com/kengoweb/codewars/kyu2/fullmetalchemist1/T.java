ce8020b32f2067314eb17501537c8972bc5b4f3c
Dmytro Turkov <dmytrot@tabtale.com>
2019-08-09 18:14:11 +0300

    codewars

--- src/com/kengoweb/codewars/kyu2/fullmetalchemist1/T.java
+++ src/com/kengoweb/codewars/kyu2/fullmetalchemist1/T.java
@@ -1,4 +1,14 @@
 package com.kengoweb.codewars.kyu2.fullmetalchemist1;
 
 public class T {
+    public int c1, c2, b1, b2, nc, nb;
+    public String elt = "";
+
+    public T(int a, int b, int c, int d) { c1 = a; b1 = b; c2 = c; b2 = d; }
+    public T(int a, int b, String c)     { nc = a; nb = b; elt = c; }
+
+    @Override public String toString() {
+        return elt.isEmpty() ? String.format("T(%d, %d, %d, %d)", c1, b1, c2, b2)
+                : String.format("T(%d, %d, %s)", nc, nb, elt);
+    }
 }
