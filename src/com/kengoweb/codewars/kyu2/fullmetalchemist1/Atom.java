da83079e6fb547ae9dda61f27abb7ec26b482e71
Dmytro Turkov <dmytrot@tabtale.com>
2019-08-09 18:13:38 +0300

    codewars

--- src/com/kengoweb/codewars/kyu2/fullmetalchemist1/Atom.java
+++ src/com/kengoweb/codewars/kyu2/fullmetalchemist1/Atom.java
@@ -1,4 +1,21 @@
 package com.kengoweb.codewars.kyu2.fullmetalchemist1;
 
 public class Atom {
+    public int id;
+    public String element;
+
+    public Atom(String elt, int id_) {
+        element = elt;
+        id = id_;
+    }
+
+    @Override public int hashCode() { return id; }        //  Do not modify this method!!
+
+    @Override public boolean equals(Object other) {       //  Do not modify this method!!
+        if (other != null && other instanceof Atom) {
+            Atom that = (Atom) other;
+            return id == that.id;
+        }
+        return false;
+    }
 }
