be8ddc22c5bd6e65a2203e0ffcf5fc9174c01c06
Dmytro Turkov <dmytrot@tabtale.com>
2019-08-09 18:12:35 +0300

    codewars

---
+++ src/com/kengoweb/codewars/kyu2/fullmetalchemist1/InvalidBond.java
@@ -0,0 +1,4 @@
+package com.kengoweb.codewars.kyu2.fullmetalchemist1;
+
+public class InvalidBond extends Exception {
+}
---
+++ src/com/kengoweb/codewars/kyu7/thepoetandthependulum/ThePoetAndThePendulum.java
@@ -0,0 +1,4 @@
+package com.kengoweb.codewars.kyu7.thepoetandthependulum;
+
+public class ThePoetAndThePendulum {
+}
